fef5041ac0c6f88f38d1bf429589aa532821cf79
paulo_infante <paulo_infante@hotmail.com>
2022-02-08 12:06:22 -0300

    add atividade_course_js-primeiros_passos_variaveis

---
+++ area_perimetro_retangulo.js
@@ -0,0 +1,9 @@
+const base = 5;
+let height = 8;
+const area = base * height;
+const perimeter = 2 * (base + height);
+//P = 2 * (c + l)
+
+console.log(area);
+console.log(perimeter);
+console.log("PARABÉNS!! VOCÊ CONSEGUIU!!!");
